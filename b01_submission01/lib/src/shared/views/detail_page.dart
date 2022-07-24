aca140f6eb5bc195e40ad6fcb56e16d4c240db0b
mirzayogy <mirza.yogy@gmail.com>
2022-07-24 15:26:43 +0800

    b01 tampil detail

--- b01_submission01/lib/src/shared/views/detail_page.dart
+++ b01_submission01/lib/src/shared/views/detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:b01_submission01/src/shared/classes/restaurant.dart';
+import 'package:b01_submission01/src/shared/views/image_clipper.dart';
 import 'package:flutter/material.dart';
 
 class RestaurantDetailPage extends StatelessWidget {
@@ -12,17 +13,25 @@ class RestaurantDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(restaurant.name),
-      ),
+          // title: Text(restaurant.name),
+          ),
       body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16.0,
+        ),
         child: Column(
           children: [
-            Image.network(restaurant.pictureId),
+            ClippedImage(
+              restaurant.pictureId,
+            ),
             Padding(
               padding: const EdgeInsets.all(10),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  const SizedBox(
+                    height: 16,
+                  ),
                   Text(
                     restaurant.name,
                     style: const TextStyle(
@@ -31,19 +40,61 @@ class RestaurantDetailPage extends StatelessWidget {
                       fontSize: 24,
                     ),
                   ),
-                  // const Divider(color: Colors.grey),
-                  // Text('Date: ${Restaurant.publishedAt}'),
-                  // const SizedBox(height: 10),
-                  // Text('Author: ${Restaurant.author}'),
-                  // const Divider(color: Colors.grey),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    children: [
+                      const Icon(
+                        Icons.location_on,
+                        size: 12,
+                      ),
+                      const SizedBox(
+                        width: 6,
+                      ),
+                      Text(restaurant.city),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const Text(
+                    "Deskripsi",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
                   Text(
                     restaurant.description,
-                    style: const TextStyle(fontSize: 16),
+                    style: const TextStyle(
+                        fontSize: 16,
+                        color: Color.fromARGB(255, 105, 104, 104)),
                   ),
                   const SizedBox(height: 10),
-                  ElevatedButton(
-                    child: const Text('Read more'),
-                    onPressed: () {},
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const Text(
+                    "Menu",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    restaurant.description,
+                    style: const TextStyle(
+                        fontSize: 16,
+                        color: Color.fromARGB(255, 105, 104, 104)),
                   ),
                 ],
               ),
